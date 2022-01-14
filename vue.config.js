b4158f8fe157f1fadd8f3fe7d60f2c0304cfdc9b
dimitarkolev <dk@almero.bg>
2022-01-14 09:04:16 +0000

    Vue cli fix

--- vue.config.js
+++ vue.config.js
@@ -9,8 +9,6 @@ module.exports = (domainConfig) => {
 	if (process.env.NODE_ENV == 'development') {
 		const machineId = machineIdSync() + '_' + os.hostname() + '_' + os.userInfo().username
 		process.env.VUE_APP_WHITEBOX_CONTEXT = machineId
-	} else {
-		process.env.VUE_APP_WHITEBOX_CONTEXT = 'mikser'
 	}
 
 	return {
