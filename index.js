a8428d1e86f1b40041ee34fad973088f2c23089c
dimitarkolev <dk@almero.bg>
2022-01-19 14:50:19 +0000

    Current fix

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-import { markdown } from './lib/markdown'
-import { metatext, removeMetatext } from './lib/metatext'
 import { mikserApp } from './lib/mikser'
 import { mikserMixin } from './lib/mixin'
 import { mikserRouter } from './lib/router'
 import { mikserStore } from './lib/store'
 
 export {
-    markdown,
-    metatext, 
-    removeMetatext,
     mikserApp,
     mikserRouter,
     mikserStore,
